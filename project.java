c849a8ac3cda2445e20467d47b802627aab5c5eb
GoldenNindroid <snowdeep72@gmail.com>
2026-08-01 15:45:48 -0700

    Add volume calculation classes for prism and cube

--- project.java
+++ project.java
@@ -1,5 +1,51 @@
 package codingal.SecondSetOfClasses.classNine;
 
-public class project {
+class volume{
+
+    public double getvolume(){
+        return 0;
+    }
     
+}
+
+
+class prism extends volume{
+    private double base;
+    private double height;
+    private double length;
+    public prism(double base, double height, double length){
+        this.base = base;
+        this.height = height;
+        this.length = length;
+    }
+
+    public double getvolume() {
+        return 0.5 * base * height * length;
+    }    
+}
+
+class cube extends volume{
+    private double side;
+    public cube(double side) {
+        this.side = side;
+    }
+
+    public double getvolume(){
+        return side * side * side;
+    }
+
+}
+
+class Main{
+    public static void main(String[] args) {
+        
+    
+    volume[] obj = new volume[2];
+
+    obj[0] = new prism(7, 5, 2);
+    obj[1] = new cube(4);
+
+    System.out.println("Volume of triangle = " + obj[0].getvolume());
+    System.out.println("Volume of square = " + obj[1].getvolume());
+    }
 }
